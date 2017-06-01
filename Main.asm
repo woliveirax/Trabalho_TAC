1f2d0a766397be5470ea79e8bc24b354382d05ca
unknown <wallace9655@gmail.com>
2017-06-01 15:48:18 +0100

    Need to fix obtem_string macro...

--- Main.asm
+++ Main.asm
@@ -8,15 +8,24 @@ dseg	segment para public 'data'
 		POSx		db	10	; POSx pode ir [1..80]
 
 		;####################################################################################################################
-		;Variaveis relativas ao MENU ↓
+		;Variaveis relativas ao MENUS
 		menu 			db	80 dup ('_'),13,10,10
-						db	'                   	 	THE MAZE GAME!',13,10,10
+						db	'                   	 	L A B I R I N T O!',13,10,10
 						db	80 dup ('_'),13,10,10
-    					db 	'				1. Jogar!',13,10,10
-						db 	'				2. Jogo com cheats!',13,10,10
-						db 	'				3. Criar labirinto!',13,10,10
-						db 	'				4. TOP 10!',13,10,10
-						db 	'				5. Sair.',10,10,10
+    					db 	'			  1. Jogar!',13,10,10
+						db 	'			  2. Configuracoes do jogo!',13,10,10
+						db 	'			  3. TOP 10!',13,10,10
+						db 	'			  4. Sair.',10,10,10
+						db	'Selecione uma opcao: ',13,10
+						db	'$',0
+		
+		menuLabirintos	db	80 dup ('_'),13,10,10
+						db  '				Configuracoes do Jogo',13,10,10
+						db	80 dup ('_'),13,10,10
+    					db 	'			  1. Carregar Labirinto por omissao.',13,10,10
+						db 	'			  2. Criar Labirinto.',13,10,10
+						db 	'			  3. Editar Labirinto.',13,10,10
+						db 	'			  4. Voltar.',10,10,10
 						db	'Selecione uma opcao: ',13,10
 						db	'$',0
 
@@ -30,10 +39,19 @@ dseg	segment para public 'data'
 		contador			db	?
 
 		;Variaveis para gestão do ficheiro de labirinto
-		fname			db	'Teste.txt',0
 		fhandle			dw	0
-		buffer			db	2 dup(32)
+		buffer			dw	?
+		
+		;####################################################################################################################
+		msgAskFich		db	"Indique o nome do ficheiro $",0
+		msgAskNovoFich	db	"Indique o nome do novo ficheiro $",0
+		msgAskPlayer 	db	"Indique o nome do jogador $",0
 
+		fname	db 12
+   				db ?
+   				db 12 dup(0)
+		
+		
 		msgErrorOpen	db	"Ocorreu um erro na abertura do fichero!$",0
 		msgErrorCreate	db	"Ocorreu um erro na criacao do ficheiro!$",0
 		msgErrorWrite	db	"Ocorreu um erro na escrita para ficheiro!$",0
@@ -62,18 +80,18 @@ assume		cs:cseg, ds:dseg
 
 LE_TECLA	PROC
 
-		mov		ah,08h		;instrução para ler a tecla.
-		int		21h			;interrupt a iniciar a condição indicada em ah.
-		mov		ah,0		;flag para saber se o número é extendido ou não.
-		cmp		al,0		;compara al para saber se o número é extendido.
-		jne		SAI_TECLA
-		mov		ah, 08h		;se for extendido volta a ler o número do buffer.
-		int		21h			;interrupt a iniciar a condição indicada em ah.
-		mov		ah,1		;indica que o número é extendido.
+			mov		ah,08h		;instrução para ler a tecla.
+			int		21h			;interrupt a iniciar a condição indicada em ah.
+			mov		ah,0		;flag para saber se o número é extendido ou não.
+			cmp		al,0		;compara al para saber se o número é extendido.
+			jne		SAI_TECLA
+			mov		ah, 08h		;se for extendido volta a ler o número do buffer.
+			int		21h			;interrupt a iniciar a condição indicada em ah.
+			mov		ah,1		;indica que o número é extendido.
 
-SAI_TECLA:
-		mov tecla,al
-		ret					;retorna da procedure
+	SAI_TECLA:
+			mov tecla,al
+			ret					;retorna da procedure
 LE_TECLA	endp
 
 ;########################################################################
@@ -93,43 +111,50 @@ endm
 ;########################################################################
 
 apaga_ecran	proc
-		xor		bx,bx
-		mov		cx,25*80
+			xor		bx,bx
+			mov		cx,25*80
 
-apaga:
-		mov		byte ptr es:[bx],' '
-		mov		byte ptr es:[bx+1],7
-		inc		bx
-		inc 	bx
-		loop	apaga
-		ret
+	apaga:
+			mov		byte ptr es:[bx],' '
+			mov		byte ptr es:[bx+1],7
+			inc		bx
+			inc 	bx
+			loop	apaga
+			ret
 apaga_ecran	endp
 
 ;########################################################################
-; 								Mostra Menu
-;########################################################################
-
-display_menu proc
-	;apaga ecra e posiciona o cursor no inicio.
-	call apaga_ecran
-	goto_xy 0,0
-
-	;mostra menu
-	mov  ah,09h
-  	lea  dx,menu
-  	int  21h
-	
-	;Pede input ao utilizador.
-	goto_xy	21,19
-	call LE_TECLA	;obtem tecla e poe em AL
-	mov tecla, al	;move a tecla para AL
-
-FIM:
-	ret
-display_menu endp
-
-;########################################################################
 ;							Funcoes do jogo
+;########################################################################
+
+obtem_string macro str
+
+	call apaga_ecran
+	goto_xy	24,10
+	mov ah,09h
+	lea dx,str
+	int 21h
+	goto_xy	34,11
+
+	mov ah, 0Ah
+	mov dx,offset fname
+	int 21h
+
+  ;CHANGE CHR(12) BY '$'.
+	mov si, offset fname + 1 ;NUMBER OF CHARACTERS ENTERED.
+	mov cl, [si] ;MOVE LENGTH TO CL.
+	mov ch, 0      ;CLEAR CH TO USE CX. 
+	inc cx ;TO REACH CHR(6).
+	add si, cx ;NOW SI POINTS TO CHR(12).
+	mov al, '$'
+	mov [si], al ;REPLACE CHR(12) BY '$'.            
+
+  ;DISPLAY STRING.                   
+            ;mov ah, 9 ;SERVICE TO DISPLAY STRING.
+            ;mov dx, offset fname + 2 ;MUST END WITH '$'.
+            ;int 21h
+endm
+
 ;########################################################################
 ;Procedure do jogo normal!
 
@@ -142,146 +167,43 @@ game proc
 game endp
 
 ;########################################################################
-;Procedure do jogo com cheats!
-
-game_cheats proc
-	mov ah,09h
-	lea dx,game_cheats_placeholder
-	int 21h
-
-	ret
-game_cheats endp
-
-;########################################################################
 ;Procedure para criar labirinto!
 
-guarda_buffer 	proc
-		push ax
+guarda_labirinto proc
 		
-		mov	bx, 360
-		mov	cx, 800	; Linhas x Colunas
+		mov		ax,0B800h ;0B800h -> endereço para memoria de video 
+		mov		es,ax	  ;colocado em es -> aponta para um sitio menos cs ds ss
 
-		xor si,si
-		mov	contador,0
 		
-		jmp Obtem_e_escreve
+		mov	ah, 3ch			; abrir ficheiro para escrita 
+		mov	cx, 00H			; tipo de ficheiro
+		lea	dx, fname		; dx contem endereco do nome do ficheiro 
+		int	21h				; abre efectivamente e AX vai ficar com o Handle do ficheiro
+		mov fhandle,ax
 
-	gambiarra:
-		add	bx,80
-		mov	contador,0
+		mov si,320
 
-	Obtem_e_escreve:
-		mov al, es:[bx]
-		mov buffer, al
-
-		mov	al, es:[bx+1]
-		mov	buffer, al
+	ciclo:
+		mov ax,es:[si]
+		add si,2
 		
-		inc si
-		inc si
+		mov buffer,ax
+		mov	bx, fhandle		; para escrever BX deve conter o Handle 
+		mov	ah, 40h			; indica que vamos escrever 
+			
+		lea	dx, buffer		;ax ->al Vamos escrever o que estiver no endereço DX
+		mov	cx, 2			;2 vamos escrever multiplos bytes duma vez só
+		int	21h				; faz a escrita 	
 
-		inc	bx
-		inc bx
+		cmp si,3840
+		jne ciclo
 
-		inc contador
-
-		cmp	contador,40
-		je gambiarra
-
-		loop Obtem_e_escreve
-	fim:
-		pop ax
+		mov	ah,3eh			; indica que vamos fechar
+		int	21h				; fecha mesmo
+							; se não acontecer erro termina
+		call apaga_ecran
 		ret
-guarda_buffer	endp
-
-save_to_file	proc
-	call apaga_ecran
-	call guarda_buffer
-
-	mov	ah, 3ch			; abrir ficheiro para escrita
-	mov	cx, 0			; tipo de ficheiro
-	lea	dx, fname		; dx contem endereco do nome do ficheiro
-	int	21h				; abre efectivamente e AX vai ficar com o Handle do ficheiro
-	jnc	escreve			; se não acontecer erro vai vamos escrever
-
-	mov	ah, 09h			; Aconteceu erro na leitura
-	lea	dx, msgErrorCreate
-	int	21h
-	jmp	fim
-
-escreve:
-
-	mov	bx, ax		; para escrever BX deve conter o Handle
-	mov	ah, 40h			; indica que vamos escrever
-	lea	dx, buffer		; Vamos escrever o que estiver no endereço DX
-	mov	cx, 1600		; vamos escrever multiplos bytes duma vez só
-	int	21h				; faz a escrita
-	
-	jnc close			; se não acontecer erro fecha o ficheiro
-
-	mov	ah, 09h
-	lea	dx, msgErrorWrite
-	int	21h
-	jmp	fim
-
-close:
-	mov	ah,3eh			; indica que vamos fechar
-	int	21h				; fecha mesmo
-	jnc	fim				; se não acontecer erro termina
-
-	mov	ah, 09h
-	lea	dx, msgErrorClose
-	int	21h
-
-fim:
-	ret
-
-save_to_file	endp
-
-
-draw_limits	proc
-		mov contador,2
-
-loop_rows:
-		goto_xy 20,contador
-
-		mov		ah, 02h
-		mov		dl, 219
-		int		21H
-		
-		goto_xy	62,contador
-
-		mov		ah, 02h
-		mov		dl, 219
-		int		21h
-
-		inc		contador
-
-		cmp		contador,24
-		jne		loop_rows
-
-		mov contador,20
-
-loop_columns:
-		goto_xy contador,2
-
-		mov		ah, 02h
-		mov		dl, 219
-		int		21H
-
-		goto_xy contador,24
-
-		mov		ah, 02h
-		mov		dl, 219
-		int		21H
-
-		inc 	contador
-		
-		cmp		contador,63
-		jne		loop_columns
-
-		ret
-draw_limits endp
+guarda_labirinto endp
 
 draw_instruct	proc
 		goto_xy 0,0
@@ -293,111 +215,261 @@ draw_instruct	proc
 draw_instruct	endp
 
 cria_labirinto proc
+			obtem_string msgAskNovoFich
+			
+			call	apaga_ecran
+			call 	draw_instruct
+			
+			mov	POSx,20
+			mov POSy,3
+			
+	CICLO:	
+			goto_xy POSx,POSy
+
+	IMPRIME:
+			mov		ah, 02h
+			mov		dl, Car
+			int		21H
+			goto_xy	POSx,POSy
+			
+			call 	LE_TECLA
+
+			cmp		ah, 1
+			je		ESTEND
+			cmp 	al, 27			; ESCAPE
+			je		fim
+
+	UM:		cmp 	al, 49			; Tecla 1
+			jne		DOIS
+			mov		Car, 219		;Caracter CHEIO
+			jmp		CICLO		
 		
-		call	apaga_ecran
-		call 	draw_instruct
-		call	draw_limits
-		
-		mov	POSx,21
-		mov POSy,3
-		
-CICLO:	
+	DOIS:	
+			cmp 	al, 50			; Tecla 2
+			jne		TRES
+			mov		Car, 178		;CINZA 178 ▓
+			jmp		CICLO			
+			
+	TRES:	
+			cmp 	al, 51			; Tecla 3
+			jne		QUATRO
+			mov		Car, 177		;CINZA 177▒
+			jmp		CICLO
+			
+	QUATRO:	
+			cmp 	al, 52			; Tecla 4
+			jne		CINCO
+			mov		Car, 176		;CINZA 176
+			jmp		CICLO
 
-IMPRIME:
-		mov		ah, 02h
-		mov		dl, Car
-		int		21H
-		goto_xy	POSx,POSy
-		
-		call 	LE_TECLA
-
-		cmp		ah, 1
-		je		ESTEND
-		cmp 	al, 27			; ESCAPE
-		je		fim
-
-UM:		cmp 	al, 49			; Tecla 1
-		jne		DOIS
-		mov		Car, 219		;Caracter CHEIO
-		jmp		CICLO		
-	
-DOIS:	
-		cmp 	al, 50			; Tecla 2
-		jne		TRES
-		mov		Car, 178		;CINZA 178 ▓
-		jmp		CICLO			
-		
-TRES:	
-		cmp 	al, 51			; Tecla 3
-		jne		QUATRO
-		mov		Car, 177		;CINZA 177▒
-		jmp		CICLO
-		
-QUATRO:	
-		cmp 	al, 52			; Tecla 4
-		jne		CINCO
-		mov		Car, 176		;CINZA 176
-		jmp		CICLO
-
-CINCO:	
-		cmp 	al, 53			; Tecla 5
-		jne		GUARDA
-		mov		Car, 32			; espaço
-		jmp		CICLO	
+	CINCO:	
+			cmp 	al, 53			; Tecla 5
+			jne		GUARDA
+			mov		Car, 32			; espaço
+			jmp		CICLO	
 
 
-GUARDA:	
-		cmp		al,103
-		jne		NOVE
-		call	save_to_file
-		jmp		fim
+	GUARDA:	
+			cmp		al,103
+			jne		NOVE
+			call	guarda_labirinto
+			jmp		fim
 
-NOVE:
-		jmp		CICLO
+	NOVE:
+			jmp		CICLO
 
-ESTEND:
-		cmp 	al,48h
-		jne		BAIXO
-		
-		cmp		POSy,3
-		je		CICLO
+	ESTEND:
+			cmp 	al,48h
+			jne		BAIXO
+			
+			cmp		POSy,3
+			je		CICLO
 
-		dec		POSy			;cima
-		jmp		CICLO
+			dec		POSy			;cima
+			jmp		CICLO
 
-BAIXO:
-		cmp		al,50h
-		jne		ESQUERDA
+	BAIXO:
+			cmp		al,50h
+			jne		ESQUERDA
 
-		cmp		POSy,23
-		je		CICLO
+			cmp		POSy,22
+			je		CICLO
 
-		inc 	POSy			;Baixo
-		jmp		CICLO
+			inc 	POSy			;Baixo
+			jmp		CICLO
 
-ESQUERDA:
-		cmp		al,4Bh
-		jne		DIREITA
+	ESQUERDA:
+			cmp		al,4Bh
+			jne		DIREITA
 
-		cmp		POSx,21
-		je		CICLO
+			cmp		POSx,20
+			je		CICLO
 
-		dec		POSx			;Esquerda
-		jmp		CICLO
+			dec		POSx			;Esquerda
+			jmp		CICLO
 
-DIREITA:
-		cmp		al,4Dh
-		jne		CICLO
+	DIREITA:
+			cmp		al,4Dh
+			jne		CICLO
 
-		cmp		POSx,61
-		je		CICLO
+			cmp		POSx,59
+			je		CICLO
 
-		inc		POSx			;Direita
-		jmp		CICLO
+			inc		POSx			;Direita
+			jmp		CICLO
 
-fim:
-		ret
+	fim:
+			ret
 cria_labirinto endp
+
+;########################################################################
+;Procedure para mostrar labirinto!
+
+edita_labirinto proc
+			obtem_string msgAskFich
+
+			call	apaga_ecran
+			call 	abre_labirinto
+			call 	draw_instruct
+			
+			mov	POSx,20
+			mov POSy,3
+			
+	CICLO:	
+			goto_xy POSx,POSy
+
+	IMPRIME:
+			mov		ah, 02h
+			mov		dl, Car
+			int		21H
+			goto_xy	POSx,POSy
+			
+			call 	LE_TECLA
+
+			cmp		ah, 1
+			je		ESTEND
+			cmp 	al, 27			; ESCAPE
+			je		fim
+
+	UM:		cmp 	al, 49			; Tecla 1
+			jne		DOIS
+			mov		Car, 219		;Caracter CHEIO
+			jmp		CICLO		
+		
+	DOIS:	
+			cmp 	al, 50			; Tecla 2
+			jne		TRES
+			mov		Car, 178		;CINZA 178 ▓
+			jmp		CICLO			
+			
+	TRES:	
+			cmp 	al, 51			; Tecla 3
+			jne		QUATRO
+			mov		Car, 177		;CINZA 177▒
+			jmp		CICLO
+			
+	QUATRO:	
+			cmp 	al, 52			; Tecla 4
+			jne		CINCO
+			mov		Car, 176		;CINZA 176
+			jmp		CICLO
+
+	CINCO:	
+			cmp 	al, 53			; Tecla 5
+			jne		GUARDA
+			mov		Car, 32			; espaço
+			jmp		CICLO	
+
+
+	GUARDA:	
+			cmp		al,103
+			jne		NOVE
+			call	guarda_labirinto
+			jmp		fim
+
+	NOVE:
+			jmp		CICLO
+
+	ESTEND:
+			cmp 	al,48h
+			jne		BAIXO
+			
+			cmp		POSy,3
+			je		CICLO
+
+			dec		POSy			;cima
+			jmp		CICLO
+
+	BAIXO:
+			cmp		al,50h
+			jne		ESQUERDA
+
+			cmp		POSy,22
+			je		CICLO
+
+			inc 	POSy			;Baixo
+			jmp		CICLO
+
+	ESQUERDA:
+			cmp		al,4Bh
+			jne		DIREITA
+
+			cmp		POSx,20
+			je		CICLO
+
+			dec		POSx			;Esquerda
+			jmp		CICLO
+
+	DIREITA:
+			cmp		al,4Dh
+			jne		CICLO
+
+			cmp		POSx,59
+			je		CICLO
+
+			inc		POSx			;Direita
+			jmp		CICLO
+
+	fim:
+			ret
+edita_labirinto endp
+
+;########################################################################
+;Procedure para ler labirinto para o ecrã
+
+abre_labirinto proc
+		mov     ah,3dh
+		mov     al,0
+		lea     dx,fname 		;ALTEREI AQUI
+		int     21h				; Chama a rotina de abertura de ficheiro (AX fica com Handle)
+		mov     fhandle,ax
+
+		
+		mov si,320
+
+	ciclo:	
+		mov    	ah, 3fh
+		mov     bx, fhandle
+		mov     cx, 2			; vai ler 2 byte de cada vez
+		lea     dx, buffer		; DX fica a apontar para o caracter lido
+		int     21h				; le 2 caracteres do ficheiro
+		mov		ax,buffer
+		
+		mov 	es:[si],ax
+		add		si,2
+
+		cmp si,3840
+		jne ciclo
+
+	fecha_ficheiro:
+		mov     ah,3eh
+		mov     bx,fhandle
+		int     21h
+
+		ret
+
+abre_labirinto endp
+
 
 ;########################################################################
 ;Procedure para mostrar top 10
@@ -422,99 +494,100 @@ altera_top10 proc
 altera_top10 endp
 
 ;########################################################################
-;Procedure para ler labirinto para o ecrã
+;									Menus
+;########################################################################
 
-preenche_buffer	proc
+display_options_menu proc
+	menu_loop:
+			call apaga_ecran
+			goto_xy 0,0
 
-		mov	ah,3Dh
-		lea	dx,fname		
-		mov al,0
-		int 21h
-		jnc	guarda
+			;mostra menu
+			mov  ah,09h
+			lea  dx,menuLabirintos
+			int  21h
+			
+			;Pede input ao utilizador.
+			goto_xy	21,17
+			call LE_TECLA	;obtem tecla e poe em AL
+			mov tecla, al
 
-		mov ah,09h
-		lea	dx,msgErrorOpen
-		int 21h
+			cmp tecla,49
+			je	carrega_lab
 
-	guarda:
-		mov fhandle,ax
+			cmp tecla,50
+			je	cria_lab
 
-		mov ah,3Fh
-		mov bx,ax
-		lea dx,buffer
-		mov cx,800
-		int 21h
-		jc erro_leitura
+			cmp tecla,51
+			je	edita_lab
 
-	erro_leitura:
-		mov ah,09h
-		lea	dx,msgErrorRead
-		int 21h
+			cmp	tecla,52
+			je	fim
+			
+			jmp menu_loop
 
-	fecha:
-		mov	ah,3eh
-		mov bx,fhandle
-		int 21h
-		jnc fim
+	carrega_lab:
+			;all carrega_lab_omissao
 
-		mov ah,09h
-		lea dx,msgErrorClose
-		int 21h
+	cria_lab:
+			call cria_labirinto
+			jmp	menu_loop
+
+	edita_lab:
+			call edita_labirinto
+			jmp menu_loop
+	
+	fim:
+			ret
+
+display_options_menu endp
+
+main_menu proc
+
+	menu_loop:
+			;apaga ecra e posiciona o cursor no inicio.
+			call apaga_ecran
+			goto_xy 0,0
+
+			;mostra menu
+			mov  ah,09h
+			lea  dx,menu
+			int  21h
+			
+			;Pede input ao utilizador.
+			goto_xy	21,17
+			call LE_TECLA	;obtem tecla e poe em AL
+			mov tecla, al	;move a tecla para AL	
+
+			cmp	tecla,49
+			je	gameNormal
+
+			cmp tecla,50
+			je	opcoes
+
+			cmp tecla,51
+			je	topTen
+
+			cmp tecla,52
+			je	fim
+			jmp menu_loop
+
+	gameNormal:
+			call game
+			call LE_TECLA
+			jmp	menu_loop
+
+	opcoes:
+			call display_options_menu
+			jmp menu_loop
+
+	topTen:
+			obtem_string msgAskPlayer
+			jmp menu_loop
 
 	fim:
-		ret
-
-preenche_buffer	endp
-
-
-insere_buffer 	proc
-	call preenche_buffer
-	
-	mov	bx, 360
-	mov	cx, 800	; Linhas x Colunas
-
-	xor si,si
-	mov	contador,0
-	
-	jmp Obtem_e_escreve
-
-gambiarra:
-	add	bx,80
-	mov	contador,0
-
-Obtem_e_escreve:
-	
-	mov al,buffer[si]
-	mov byte ptr es:[bx],al
-	
-	mov	al,buffer[si+1]
-	mov	byte ptr es:[bx+1],al
-	
-	inc si
-	inc si
-
-	inc	bx
-	inc bx
-
-	inc contador
-
-	cmp	contador,40
-	je gambiarra
-
-	loop Obtem_e_escreve
-
-fim:
-	ret
-insere_buffer	endp
-
-
-abre_labirinto proc
-	call apaga_ecran
-	call preenche_buffer
-	call insere_buffer
-
-	ret
-abre_labirinto endp
+			ret
+main_menu endp
 
 ;########################################################################
 ;									Main
@@ -527,49 +600,11 @@ Main  proc
 		mov 	ax,0B800h	;move o ponteiro para memoria de video para ax
 		mov		es,ax		;move o ponteiro para memoria de video de ax para ES
 
-menu_loop:
-		
-		call display_menu	;mostra menu
+		call main_menu
 
-		cmp	tecla,49
-		je	gameNormal
-		
-		cmp tecla,50
-		je	gameCheats
-
-		cmp tecla,51
-		je	criaLab
-
-		cmp tecla,52
-		je	topTen
-
-		cmp tecla,53
-		je	fim
-		jmp menu_loop
-
-gameNormal:
-		call game
-		call LE_TECLA
-		jmp	menu_loop
-
-gameCheats:
-		call game_cheats
-		call LE_TECLA
-		jmp menu_loop
-
-criaLab:
-		call cria_labirinto
-		call LE_TECLA
-		jmp menu_loop
-
-topTen:
-		call abre_labirinto
-		call LE_TECLA
-		jmp menu_loop
-
-fim:
+	fim:
 		mov		ah,4CH
-		INT		21H
+		int		21H
 
 Main	endp
 Cseg	ends
