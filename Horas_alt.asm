a0f34dab2bab35164d8e6b0899fc213209b55c8a
unknown <wallace9655@gmail.com>
2017-06-04 00:32:37 +0100

    Funciona

--- Horas_alt.asm
+++ Horas_alt.asm
@@ -36,3 +36,178 @@ MOSTRA MACRO str
 
 endm
 
+;########################################################################
+;                          Rotinas de TEMPO 
+;########################################################################
+
+Ler_TEMPO PROC	
+ 
+		PUSH AX
+		PUSH BX
+		PUSH CX
+		PUSH DX
+	
+		PUSHF
+		
+		MOV AH, 2CH             ; Buscar a hORAS
+		INT 21H                 
+		
+		XOR AX,AX
+		MOV AL, DH              ; segundos para al
+		mov Segundos, AX		; guarda segundos na variavel correspondente
+		
+		XOR AX,AX
+		MOV AL, CL              ; Minutos para al
+		mov Minutos, AX         ; guarda MINUTOS na variavel correspondente
+		
+		XOR AX,AX
+		MOV AL, CH              ; Horas para al
+		mov Horas,AX			; guarda HORAS na variavel correspondente
+ 
+		POPF
+		POP DX
+		POP CX
+		POP BX
+		POP AX
+ 		RET 
+Ler_TEMPO ENDP 
+
+;************************************************************************
+; LEITURA DE UMA TECLA DO TECLADO 
+; LE UMA TECLA	E DEVOLVE VALOR EM AH E AL
+; SE ah=0 É UMA TECLA NORMAL
+; SE ah=1 É UMA TECLA EXTENDIDA
+; AL DEVOLVE O CÓDIGO DA TECLA PREMIDA
+;************************************************************************
+
+LE_TECLA proc	
+
+		cmp	ah,0
+		je	espera_tecla
+
+	mostra_horas:
+			call Trata_Horas
+			
+			mov	ah,0Bh
+			int 21h
+			
+			cmp al,0
+			je	mostra_horas
+
+			goto_xy	POSx,POSy
+
+	espera_tecla:
+			mov	ah,08h
+			int	21h
+			mov	ah,0
+
+			cmp	al,0
+			jne	SAI_TECLA
+			
+			mov	ah, 08H
+			int	21h
+			
+			mov	ah,1
+	SAI_TECLA:	
+			ret
+
+LE_TECLA endp
+
+;########################################################################
+;			  ---	começa a contar o tempo de jogo do zero  ---
+;########################################################################
+
+; aqui incrementa o tempo a começar do zero;
+Incrementa_Segundos PROC
+		
+	inc_segundos:
+
+			cmp Game_Time_s, 60
+			je inc_minutos
+			inc Game_Time_s
+			jmp Fim
+			
+	inc_minutos:
+			inc Game_Time_m
+			mov Game_Time_s, 0
+			cmp Game_Time_m, 60
+			je inc_horas
+			jmp Fim
+			
+	inc_horas:
+			mov Game_Time_s,0
+			mov Game_Time_m,0
+			jmp Fim
+	Fim:
+		ret
+		
+Incrementa_Segundos ENDP
+
+;########################################################################
+;			  ---	começa a contar o tempo de jogo do zero  ---
+;########################################################################
+Trata_Horas PROC
+
+			PUSHF
+			PUSH AX
+			PUSH BX
+			PUSH CX
+			PUSH DX		
+
+			CALL 	Ler_TEMPO			; Horas MINUTOS e segundos do Sistema
+			
+			MOV		AX, Segundos
+			cmp		AX, Old_seg			; VErifica se os segundos mudaram desde a ultima leitura
+			je		fim_horas			; Se a hora não mudou desde a última leitura sai.
+			mov		Old_seg, AX			; Se segundos são diferentes actualiza informação do tempo 
+			
+			call	incrementa_segundos ; vai contar o tempo que o utilizador demora a completar o labirinto
+			
+			mov 	ax,Game_Time_h
+			MOV		bl, 10     
+			div 	bl
+			add 	al, 30h				; Caracter Correspondente às dezenas
+			add		ah,	30h				; Caracter Correspondente às unidades
+			MOV 	STR12[0],al			; 
+			MOV 	STR12[1],ah
+			MOV 	STR12[2],'h'		
+			MOV 	STR12[3],'$'
+			GOTO_XY 66,14				; alterar a posiçao das horas coluna/linha
+			MOSTRA STR12
+			
+			mov 	ax,Game_Time_m		; variavel que vai ser incrementada atraves da funçao incrementa tempo
+			MOV 	bl, 10     
+			div 	bl
+			add 	al, 30h				; Caracter Correspondente às dezenas
+			add		ah,	30h				; Caracter Correspondente às unidades
+			MOV 	STR12[0],al			; 
+			MOV 	STR12[1],ah
+			MOV 	STR12[2],'m'		
+			MOV 	STR12[3],'$'
+			GOTO_XY	70,14				; alterar a posiçao das min coluna/linha
+			MOSTRA STR12				; mostra min 
+			
+			mov 	ax,Game_Time_s		; variavel que vai ser incrementada atraves da funçao incrementa tempo
+			MOV 	bl, 10     
+			div 	bl
+			add 	al, 30h				; Caracter Correspondente às dezenas
+			add		ah,	30h				; Caracter Correspondente às unidades
+			MOV 	STR12[0],al			; 
+			MOV 	STR12[1],ah
+			MOV 	STR12[2],'s'		
+			MOV 	STR12[3],'$'
+			GOTO_XY	74,14				; alterar a posiçao das segundos coluna/linha
+			MOSTRA STR12				; 
+								
+	fim_horas:		
+			goto_xy	POSx,POSy			; Volta a colocar o cursor onde estava antes de actualizar as horas
+			
+			POPF
+			POP DX		
+			POP CX
+			POP BX
+			POP AX
+			RET		
+			
+
+Trata_Horas ENDP
